5565f8152c74389ded2ac81dd5f54d2c0ceca26e
Bernard Zhao <bernard.zhao.us@gmail.com>
2020-11-02 01:15:38 -0800

    Updated client code

--- shared/src/main/scala/frankenpaxos/raft/Client.scala
+++ shared/src/main/scala/frankenpaxos/raft/Client.scala
@@ -1,10 +1,10 @@
 package frankenpaxos.raft
 
-import frankenpaxos.Actor
-import frankenpaxos.Logger
-import frankenpaxos.ProtoSerializer
-import frankenpaxos.Chan
+import frankenpaxos.{Actor, Chan, Logger, ProtoSerializer}
+import scala.concurrent.{Future, Promise}
 import scala.scalajs.js.annotation._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 @JSExportAll
 object ClientInboundSerializer extends ProtoSerializer[ClientInbound] {
@@ -32,73 +32,160 @@ class Client[Transport <: frankenpaxos.Transport[Transport]](
   // The set of raft nodes.
   val raftParticipants: Seq[Chan[Participant[Transport]]] =
     for (participantAddress <- config.participantAddresses)
-      yield
-        chan[Participant[Transport]](
-          participantAddress,
-          Participant.serializer
-        )
+      yield chan[Participant[Transport]](
+        participantAddress,
+        Participant.serializer
+      )
 
   // index of presumed leader
   var leaderIndex = 0
 
+  // pending action - No need for lock since client acts synchronously
+  @JSExportAll
+  sealed trait PendingState
+
+  @JSExportAll
+  case class PendingWrite(
+      promise: Promise[Boolean],
+      cmd: String
+  ) extends PendingState
+
+  @JSExportAll
+  case class PendingRead(
+      promise: Promise[String],
+      index: Integer
+  ) extends PendingState
+
+  var pending: Option[PendingState] = None
+
   logger.info(s"Raft client listening on $srcAddress.")
 
-  override def receive(src: Transport#Address, inbound: InboundMessage): Unit = {
+  override def receive(
+      src: Transport#Address,
+      inbound: InboundMessage
+  ): Unit = {
     import ClientInbound.Request
     inbound.request match {
-      case Request.WriteCmdResponse(r) => handleWriteCommandResponse(src, r)
-      case Request.ReadCmdResponse(r)  => handleReadCommandResponse(src, r)
+      case Request.ClientRequestResponse(r) =>
+        handleClientRequestResponse(src, r)
+      case Request.ClientQueryResponse(r) => handleClientQueryResponse(src, r)
       case Request.Empty => {
         logger.fatal("Empty ClientInbound encountered.")
       }
     }
   }
 
-  private def handleWriteCommandResponse(src: Transport#Address, cmdRes: WriteCommandResponse) {
-    logger.info(s"Got WriteCommandResponse from ${src}" 
-                + s"| Success: ${cmdRes.success}"
-                + s"| LeaderIndex: ${cmdRes.leaderIndex}"
-                + s"| Command: ${cmdRes.cmd}"
-                + s"| LogIndex: ${cmdRes.index}")
-            
-    if (!cmdRes.success) {
-        leaderIndex = cmdRes.leaderIndex
-        logger.info(s"$src is not leader, trying again with ${raftParticipants(leaderIndex).dst}.")
-        writeCommand(cmdRes.cmd)
-    } else {
-      logger.info(s"Command succussfully replicated!")
+  // Helpers
+
+  private def handleClientRequestResponse(
+      src: Transport#Address,
+      requestResponse: ClientRequestResponse
+  ) {
+    logger.info(
+      s"Got ClientRequestResponse from ${src}"
+        + s"| Success: ${requestResponse.success}"
+        + s"| Response: ${requestResponse.response}"
+        + s"| Leader Hint: ${requestResponse.leaderHint}"
+    )
+    pending match {
+      case Some(pendingWrite: PendingWrite) =>
+        if (!requestResponse.success) {
+          if (requestResponse.response == "NOT_LEADER") {
+            leaderIndex = requestResponse.leaderHint
+            logger.info(
+              s"$src is not leader, trying again with ${raftParticipants(leaderIndex).dst}."
+            )
+            write(pendingWrite.cmd)
+          }
+        } else {
+          logger.info("Command successfully replicated!")
+          pendingWrite.promise.success(true)
+          pending = None
+        }
+      case Some(_: PendingRead) =>
+        logger.error("Request response received while no pending read exists.")
+      case None =>
+        logger.error(
+          "Request response received while no pending action exists."
+        )
     }
   }
 
-  private def handleReadCommandResponse(src: Transport#Address, cmdRes: ReadCommandResponse) {
-    logger.info(s"Got ReadCommandResponse from ${src}" 
-                + s"| Success: ${cmdRes.success}"
-                + s"| LeaderIndex: ${cmdRes.leaderIndex}"
-                + s"| Command: ${cmdRes.cmd}"
-                + s"| LogIndex: ${cmdRes.index}")
-            
-    if (!cmdRes.success) {
-        leaderIndex = cmdRes.leaderIndex
-        logger.info(s"$src is not leader, trying again with ${raftParticipants(leaderIndex).dst}.")
-        readCommand(cmdRes.index)
-    } else {
-      logger.info(s"Read command successfully!")
+  private def handleClientQueryResponse(
+      src: Transport#Address,
+      queryResponse: ClientQueryResponse
+  ) {
+    logger.info(
+      s"Got ClientQueryResponse from ${src}"
+        + s"| Success: ${queryResponse.success}"
+        + s"| Response: ${queryResponse.response}"
+        + s"| Leader Hint: ${queryResponse.leaderHint}"
+    )
+    pending match {
+      case Some(pendingWrite: PendingWrite) =>
+        logger.error("Request response received while no pending write exists.")
+      case Some(pendingRead: PendingRead) =>
+        if (!queryResponse.success) {
+          if (queryResponse.response == "NOT_LEADER") {
+            leaderIndex = queryResponse.leaderHint
+            logger.info(
+              s"$src is not leader, trying again with ${raftParticipants(leaderIndex).dst}."
+            )
+            read(pendingRead.index)
+          }
+        } else {
+          logger.info("Command successfully replicated!")
+          pendingRead.promise.success(queryResponse.response)
+          pending = None
+        }
+      case None =>
+        logger.error(
+          "Request response received while no pending action exists."
+        )
     }
   }
 
-  private def writeCommandImpl(cmd: String): Unit = {
-    raftParticipants(leaderIndex).send(ParticipantInbound().withWriteCmdRequest(WriteCommandRequest(cmd = cmd)))
+  private def writeImpl(cmd: String): Unit = {
+    raftParticipants(leaderIndex).send(
+      ParticipantInbound().withClientRequest(ClientRequest(cmd = cmd))
+    )
   }
 
-  private def readCommandImpl(index: Int): Unit = {
-    raftParticipants(leaderIndex).send(ParticipantInbound().withReadCmdRequest(ReadCommandRequest(index = index)))
+  private def readImpl(index: Int): Unit = {
+    raftParticipants(leaderIndex).send(
+      ParticipantInbound().withClientQuery(ClientQuery(index = index))
+    )
   }
 
-  def writeCommand(cmd: String): Unit = {
-    transport.executionContext().execute(() => writeCommandImpl(cmd))
+  // Interface
+
+  def write(cmd: String): Future[Boolean] = {
+    if (pending != None) {
+      throw new Exception("An action is already pending!")
+    }
+    val promise = Promise[Boolean]()
+    pending = Some(
+      PendingWrite(
+        promise = promise,
+        cmd = cmd
+      )
+    )
+    transport.executionContext().execute(() => writeImpl(cmd))
+    promise.future
   }
 
-  def readCommand(index: Int): Unit = {
-    transport.executionContext().execute(() => readCommandImpl(index))
+  def read(index: Int): Future[String] = {
+    if (pending != None) {
+      throw new Exception("An action is already pending!")
+    }
+    val promise = Promise[String]()
+    pending = Some(
+      PendingRead(
+        promise = promise,
+        index = index
+      )
+    )
+    transport.executionContext().execute(() => readImpl(index))
+    promise.future
   }
 }
