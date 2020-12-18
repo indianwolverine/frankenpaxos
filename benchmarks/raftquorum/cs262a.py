41126f45ca7252fa3abc936b5ddb26b76d4b1f11
Ubuntu <ubuntu@ip-172-31-4-159.us-west-1.compute.internal>
2020-12-18 04:56:12 +0000

    ported changes to raftquorum

--- benchmarks/raftquorum/cs262a.py
+++ benchmarks/raftquorum/cs262a.py
@@ -11,8 +11,6 @@ def main(args) -> None:
                 Input(
                     f = f,
                     num_client_procs = num_client_procs,
-                    num_warmup_clients_per_proc = 0,
-                    num_clients_per_proc = num_clients_per_proc,
                     measurement_group_size = 1,
                     warmup_duration = datetime.timedelta(seconds=0),
                     warmup_timeout = datetime.timedelta(seconds=0),
@@ -21,17 +19,8 @@ def main(args) -> None:
                     timeout = datetime.timedelta(seconds=20),
                     client_lag = datetime.timedelta(seconds=5),
                     state_machine = 'KeyValueStore',
-                    predetermined_read_fraction = predetermined_read_fraction,
                     workload_label = "cs262a",
-                    workload = read_write_workload.UniformReadWriteWorkload(
-                        num_keys=1, read_fraction=0.5, write_size_mean=1,
-                        write_size_std=0),
-                    read_workload = read_write_workload.UniformReadWriteWorkload(
-                        num_keys=1, read_fraction=1.0, write_size_mean=1,
-                        write_size_std=0),
-                    write_workload = read_write_workload.UniformReadWriteWorkload(
-                        num_keys=1, read_fraction=0.0, write_size_mean=1,
-                        write_size_std=0),
+                    workload = workload,
                     profiled = args.profile,
                     monitored = args.monitor,
                     prometheus_scrape_interval =
@@ -52,25 +41,27 @@ def main(args) -> None:
                     client_log_level = args.log_level,
                 )
 
-                for (f, num_client_procs, num_clients_per_proc, 
-                    predetermined_read_fraction) in [
+                for (f, num_client_procs, workload) in [
                     # 3 participants
-                    (1, 1, 2, 0),
-                    (1, 1, 2, 25),
-                    (1, 1, 2, 50),
-                    (1, 1, 2, 70),
+                    (1, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0, write_size_mean=1, write_size_std=0)),
+                    (1, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.25, write_size_mean=1, write_size_std=0)),
+                    (1, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.5, write_size_mean=1, write_size_std=0)),
+                    (1, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.75, write_size_mean=1, write_size_std=0)),
+                    (1, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=1, write_size_mean=1, write_size_std=0)),
 
                     # 5 participants
-                    (2, 1, 2, 0),
-                    (2, 1, 2, 25),
-                    (2, 1, 2, 50),
-                    (2, 1, 2, 70),
+                    (2, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0, write_size_mean=1, write_size_std=0)),
+                    (2, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.25, write_size_mean=1, write_size_std=0)),
+                    (2, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.5, write_size_mean=1, write_size_std=0)),
+                    (2, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.75, write_size_mean=1, write_size_std=0)),
+                    (2, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=1, write_size_mean=1, write_size_std=0)),
 
                     # 7 participants
-                    (3, 1, 2, 0),
-                    (3, 1, 2, 25),
-                    (3, 1, 2, 50),
-                    (3, 1, 2, 70),
+                    (3, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0, write_size_mean=1, write_size_std=0)),
+                    (3, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.25, write_size_mean=1, write_size_std=0)),
+                    (3, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.5, write_size_mean=1, write_size_std=0)),
+                    (3, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=0.75, write_size_mean=1, write_size_std=0)),
+                    (3, 10, read_write_workload.UniformReadWriteWorkload(num_keys=1, read_fraction=1, write_size_mean=1, write_size_std=0)),
                 ]
             ] * 1)[:]
 
@@ -79,12 +70,11 @@ def main(args) -> None:
                 'f': input.f,
                 'num_participants': 2 * input.f + 1,
                 'num_client_procs': input.num_client_procs,
-                'num_clients_per_proc': input.num_clients_per_proc,
                 'write.latency.median_ms': \
                     f'{output.write_output.latency.median_ms:.6}',
                 'write.start_throughput_1s.p90': \
                     f'{output.write_output.start_throughput_1s.p90:.6}',
-                'read.latency.mediam_ms': \
+                'read.latency.median_ms': \
                     f'{output.read_output.latency.median_ms:.6}',
                 'read.start_throughput_1s.p90': \
                     f'{output.read_output.start_throughput_1s.p90:.8}',
